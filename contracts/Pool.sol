025dff264134a8d4e1b22d2e599e83adfe51b76b
Noro <noromeb85@gmail.com>
2022-12-11 13:05:45 +0100

    added borrow function

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -1,20 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./PoolLogic.sol";
 import "./PoolConfiguration.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "../interfaces/IXToken.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Pool is Ownable {
     PoolConfiguration public poolConfiguration;
+    PoolLogic public poolLogic;
 
     function setPoolConfigurationAddress(address _poolConfigurationAddress)
         external
         onlyOwner
     {
         poolConfiguration = PoolConfiguration(_poolConfigurationAddress);
+    }
+
+    function setPoolLogicAddress(address _poolLogicAddress) external onlyOwner {
+        poolLogic = PoolLogic(_poolLogicAddress);
     }
 
     function supply(address _asset, uint256 _amount) public {
@@ -25,8 +30,19 @@ contract Pool is Ownable {
         IXToken(xtoken).mint(msg.sender, _amount);
     }
 
-    function borrow(address _asset, uint256 _amount) public {
+    function borrow(address _asset, uint256 _amount) public returns (uint256) {
         address xtoken = poolConfiguration.underlyingAssetToXtoken(_asset);
+
+        bool isValid = poolLogic.validateBorrow(msg.sender, _asset, _amount);
+
+        if (!isValid) {
+            return 0;
+        } else {
+            IXToken(xtoken).transferUnderlyingAssetTo(msg.sender, _amount);
+            IXToken(xtoken).burn(msg.sender, _amount);
+
+            return _amount;
+        }
     }
 
     function withdraw(address _asset, uint256 _amount) public {
