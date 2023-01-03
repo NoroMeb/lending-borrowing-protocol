09b020a5f31db2fb6b986ef1d38437b96b43b95f
Noro <noromeb85@gmail.com>
2023-01-03 16:15:10 +0100

    added Natspec comments to PoolConfiguration contract

--- contracts/PoolLogic.sol
+++ contracts/PoolLogic.sol
@@ -7,6 +7,12 @@ import "./PriceOracle.sol";
 import "./PoolConfiguration.sol";
 import "@ds-math/src/math.sol";
 
+/**
+ * @author  . MEBARKIA Abdenour
+ * @title   . PoolLogic
+ * @dev     . validate some end-user functions in the Pool contract
+ */
+
 contract PoolLogic is DSMath {
     uint256 public constant maxAmountRate = 7500; //in basis points
     PoolConfiguration public poolConfiguration;
@@ -15,6 +21,12 @@ contract PoolLogic is DSMath {
         poolConfiguration = PoolConfiguration(_poolConfigurationAddress);
     }
 
+    /**
+     * @dev     . get the user xToken balance in USD
+     * @param   _account  . the user's address
+     * @param   _underlyingAsset  . the asset underlying asset's xToken
+     * @return  uint256  . returns user xToken balance in USD
+     */
     function getUserBalanceInUSD(address _account, address _underlyingAsset)
         internal
         view
@@ -35,6 +47,12 @@ contract PoolLogic is DSMath {
         return userBalanceInUSD;
     }
 
+    /**
+     * @dev     . get the user debtToken balance in USD
+     * @param   _account  . the user's address
+     * @param   _underlyingAsset  . the asset underlying asset's xToken
+     * @return  uint256  . returns user debtToken balance in USD
+     */
     function getUserDebtInUSD(address _account, address _underlyingAsset)
         internal
         view
@@ -55,6 +73,12 @@ contract PoolLogic is DSMath {
         return userDebtInUSD;
     }
 
+    /**
+     * @dev     . get an amount of an underlying asset price in USD
+     * @param   _amount  . the amount to get its price
+     * @param   _underlyingAsset  . the underlying asset address
+     * @return  uint256  . price in USD
+     */
     function getAmountInUSD(uint256 _amount, address _underlyingAsset)
         internal
         view
@@ -71,6 +95,15 @@ contract PoolLogic is DSMath {
         return amountInUSD;
     }
 
+    /**
+     * @dev     . tells if user is legitimate to borrow
+     * @param   _account  . the address of the user who wants to borrow
+     * @param   _asset  . The address of the underlying asset to borrow
+     * @param   _amount  . The amount to be borrowed
+     * @param   _collateral  . he address of the underlying asset to set as collateral
+     * @return  bool  . success boolian
+     * @return  uint256  . the amount borrowed if the user is legitimate to borrow otherwise it returns 0
+     */
     function validateBorrow(
         address _account,
         address _asset,
@@ -103,6 +136,13 @@ contract PoolLogic is DSMath {
         }
     }
 
+    /**
+     * @dev     . tells if user is legitimate to withdraw
+     * @param   _account  . the address of the user who wants to withdraw
+     * @param   _underlyingAsset  . The address of the underlying asset to withdraw
+     * @param   _amount  . The amount to be withdrawn
+     * @return  bool  . legitimacy boolian
+     */
     function validateWithdraw(
         address _account,
         address _underlyingAsset,
@@ -126,6 +166,13 @@ contract PoolLogic is DSMath {
         }
     }
 
+    /**
+     * @dev     . get the collateral amount to mint depending on an amount of asset
+     * @param   _asset  . address of the asset
+     * @param   _amount  . the amount of the asset
+     * @param   _collateral  . the address of the collateral to mint
+     * @return  uint256  . collateral amount
+     */
     function getCollateralAmountToMint(
         address _asset,
         uint256 _amount,
@@ -141,6 +188,15 @@ contract PoolLogic is DSMath {
         return amountOfAssetInUSD / collateralPrice;
     }
 
+    /**
+     * @dev     . tells if user is legitimate to liquidate a non-healthy position collateral-wise
+     * @param   _user  . The address of the borrower getting liquidated
+     * @param   _asset  . The address of the underlying borrowed asset to be repaid with the liquidation
+     * @param   _collateral  . The address of the underlying asset used as collateral, to receive as result of the liquidation
+     * @param   _collateralAmount  . the amount of collateral borrower hass
+     * @return  bool  . legitimacy boolian
+     * @return  uint256  . the amount undercollateralized
+     */
     function validateLiquidation(
         address _user,
         address _asset,
