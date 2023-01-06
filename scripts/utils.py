8c9ee8925615aacc67b42c43f84aba933a45289c
Noro <noromeb85@gmail.com>
2023-01-06 17:20:54 +0100

    added second live account to get_account

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,13 +3,15 @@ from brownie import network, accounts, config
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
 
 
-def get_account(index=None, id=None):
+def get_account(index=None, id=None, num=None):
     if index:
         return accounts[index]
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
     if id:
         return accounts.load(id)
-    if network.show_active() in config["networks"]:
+    if network.show_active() in config["networks"] and not num:
         return accounts.add(config["wallets"]["from_key"])
+    if network.show_active() in config["networks"] and num == 2:
+        return accounts.add(config["wallets"]["from_key_2"])
     return None
